1dee56746085fa9d6ba65b096f57de05b189a85b
Karim <karim.ayoub.job@gmail.com>
2026-04-15 14:19:51 +0200

    feat: Ajout de la fonctionnalité création de nouveu post

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const BlogPost = require("./src/models/blogPost.js");
 const app = express();
 
-
 // Connexion à MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/my_blog')
-  .then(() => console.log('Connexion MongoDB réussie'))
-  .catch((err) => console.error('Erreur de connexion à MongoDB:', err));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/my_blog")
+  .then(() => console.log("Connexion MongoDB réussie"))
+  .catch((err) => console.error("Erreur de connexion à MongoDB:", err));
 
 // Définir EJS comme moteur de template
 app.set("view engine", "ejs");
 
 // Dossier des fichiers statiques (CSS, JS…)
 app.use(express.static("public"));
+app.use(express.json()); // Pour parser les données JSON
+app.use(express.urlencoded({ extended: true })); // Pour parser les données des formulaires
 
 // Route principale
 app.get("/", (req, res) => {
@@ -36,6 +39,23 @@ app.get("/post", (req, res) => {
 
 app.get("/post/new", (req, res) => {
   res.render("create");
+});
+
+// Route pour créer un nouveau post
+app.post("/post/store", async (req, res) => {
+  console.log("Touch");
+  
+  const { title, body } = req.body;
+
+  BlogPost.create({ title, body })
+    .then((blogPost) => {
+      console.log("Blog post created:", blogPost);
+      res.redirect("/"); // Redirige vers la page d'accueil après la création du post
+    })
+    .catch((err) => {
+      console.error("Error creating blog post:", err);
+      res.render("create", { title: "Nouveau Post - Creations" });
+    });
 });
 
 // Lancer le serveur
