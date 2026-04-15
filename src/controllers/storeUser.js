0a8219d337bc177bebdbcde50a28072ec842b2ce
Karim <karim.ayoub.job@gmail.com>
2026-04-15 19:35:07 +0200

    feat: Message pour les champs requis coté user

--- src/controllers/storeUser.js
+++ src/controllers/storeUser.js
@@ -12,6 +12,8 @@ module.exports = async (req, res) => {
     })
     .catch((err) => {
       console.error("Error saving user:", err);
+      const validationErrors = Object.keys(err.errors).map((key) => err.errors[key].message);
+      req.session.validationErrors = validationErrors;
       return res.redirect("/auth/register");
     });
 };
