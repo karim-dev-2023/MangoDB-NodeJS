56ce5df338c1da144a868311eda88e529ab0379f
Karim <karim.ayoub.job@gmail.com>
2026-04-15 18:21:30 +0200

    Crypt user password

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
+
 const UserSchema = new Schema({
   username: String,
   password: String,  
+});
+
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = mongoose.model('User', UserSchema);
